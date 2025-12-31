eba9349be9395f85c3945374880d5d19214d6fe2
CMA <charefeddinehoggas85@gmail.com>
2025-12-31 19:44:36 +0100

    Pedagogie01

---
+++ src/api/page-pedagogie/controllers/page-pedagogie.ts
@@ -0,0 +1,7 @@
+/**
+ * page-pedagogie controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::page-pedagogie.page-pedagogie');
---
+++ src/api/page-pedagogie/services/page-pedagogie.ts
@@ -0,0 +1,7 @@
+/**
+ * page-pedagogie service
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreService('api::page-pedagogie.page-pedagogie');
