913375bbdad15de45f85a0e036fa44a262d44a18
CMA <charefeddinehoggas85@gmail.com>
2026-01-02 13:58:49 +0100

    indicateurs de perfomances

---
+++ src/api/page-indicateurs-performance/controllers/page-indicateurs-performance.ts
@@ -0,0 +1,7 @@
+/**
+ * page-indicateurs-performance controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::page-indicateurs-performance.page-indicateurs-performance');
---
+++ src/api/page-indicateurs-performance/routes/page-indicateurs-performance.ts
@@ -0,0 +1,7 @@
+/**
+ * page-indicateurs-performance router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::page-indicateurs-performance.page-indicateurs-performance');
